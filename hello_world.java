f258f6a502642afeb94e6822d88fa5c54ca8fd36
manchal-02 <67007354+manchal-02@users.noreply.github.com>
2022-06-15 14:08:41 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ public class hello_world
   public static void main (String as [])
     {
       System.out.println("Hello World !!");
-      System.out.println("Changes made to check the triggers #4 ");
+      System.out.println("Changes made to check the triggers #6 ");
     }
   }
